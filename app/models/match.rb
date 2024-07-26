f41d05ee35aa4145c24e1fa9836050e7251e44ba
yateesh-dt <yateesh.tangudu@dispatchtrack.com>
2024-07-26 13:19:27 +0530

    Validations added to matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,12 +1,12 @@
 require 'csv'
 
 class Match < ApplicationRecord
-   scope :by_team1, ->(team1_id) { where(team1_id: team1_id) }
-scope :by_team2, ->(team2_id) { where(team2_id: team2_id) }
-scope :by_date_range, ->(start_date, end_date) { where(date: start_date..end_date) }
-  belongs_to :team1, class_name: 'Team'
-  belongs_to :team2, class_name: 'Team'
-  before_save :date_check
+    scope :by_team1, ->(team1_id) { where(team1_id: team1_id) }
+    scope :by_team2, ->(team2_id) { where(team2_id: team2_id) }
+    scope :by_date_range, ->(start_date, end_date) { where(date: start_date..end_date) }
+    belongs_to :team1, class_name: 'Team'
+    belongs_to :team2, class_name: 'Team'
+    before_save :date_check
 
   def self.process_data(path)
     CSV.read(path, headers: true).map do |row|
@@ -27,6 +27,12 @@ scope :by_date_range, ->(start_date, end_date) { where(date: start_date..end_dat
       Match.create!(match_attributes)
     end
   end
+
+  validates :location, presence: true
+  validates :date, presence: true
+  validates :team1_id, presence: true
+  validates :team2_id, presence: true
+
   private
   def date_check
     if date.present? && date < Date.today
